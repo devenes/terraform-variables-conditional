00ccc695345d75a072ace352ca3b0dca0a8f1175
Enes <66560757+devenes@users.noreply.github.com>
2022-04-08 00:39:14 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ec2_name" {
+  default = "enes-ec2"
+}
+
+variable "ec2_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_ami" {
+  default = "ami-0742b4e673072066f"
+}
+
+variable "s3_bucket_name" {
+  default = "enes-s3-bucket-variable-addwhateveryouwant1"
+}
+
+variable "key_name" {
+  default = "east1"
+}
